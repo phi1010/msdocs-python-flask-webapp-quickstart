a74022997e4e68ce95aa1610af11d933bde53f78
Phillip Kuhrt <mail@phi1010.com>
2025-10-19 09:59:21 +0200

    enhance logging and add user group fetching functionality

--- app.py
+++ app.py
@@ -2,14 +2,26 @@ import base64
 import json
 import logging
 import os
-
+import sys
+import requests
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 from pprint import pformat
 
 app = Flask(__name__)
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG, stream=sys.stderr)
 logger = logging.getLogger(__name__)
+logger.debug("1: DEBUG logging is enabled")
+logger.info("1: INFO logging is enabled")
+logger.warning("1: WARNING logging is enabled")
+logger.error("1: ERROR logging is enabled")
+logger.fatal("1: FATAL logging is enabled")
+logger.setLevel(logging.DEBUG)
+logger.debug("2: DEBUG logging is enabled")
+logger.info("2: INFO logging is enabled")
+logger.warning("2: WARNING logging is enabled")
+logger.error("2: ERROR logging is enabled")
+logger.fatal("2: FATAL logging is enabled")
 
 
 @app.route('/')
@@ -18,19 +30,53 @@ def index():
     print(pformat((request, dict(request.headers), dict(os.environ))))
     for header, value in request.headers.items():
         try:
-            value = base64.b64decode(value)
+            value = base64.b64decode(value).decode('utf-8')
         except:
-            pass
-        try:
-            value = value.decode('utf-8')
-        except:
-            pass
+            try:
+                # JWT token?
+                value = base64.b64decode(value.split(".")[1]).decode("utf-8")
+            except:
+                pass
         try:
             value = json.loads(value)
         except:
             pass
         print(pformat((header, value)))
+
+    try:
+        user_token = request.headers.get("X-MS-TOKEN-AAD-ACCESS-TOKEN", "")
+        user_groups = fetchUserGroups(user_token)
+        print(f"User is member of {len(user_groups)} groups")
+        for group in user_groups:
+            print(f"  Group: {group!r}")
+    except:
+        logger.exception("Error fetching user groups")
     return render_template('index.html')
+
+
+def fetchUserGroups(userToken, nextLink=None):
+    # Recursively fetch group membership
+    if nextLink:
+        endpoint = nextLink
+    else:
+        endpoint = "https://graph.microsoft.com/v1.0/me/transitiveMemberOf?$select=id"
+
+    headers = {"Authorization": "bearer " + userToken}
+    try:
+        r = requests.get(endpoint, headers=headers)
+        if r.status_code != 200:
+            logging.error(f"Error fetching user groups: {r.status_code} {r.text}")
+            return []
+
+        r = r.json()
+        if "@odata.nextLink" in r:
+            nextLinkData = fetchUserGroups(userToken, r["@odata.nextLink"])
+            r["value"].extend(nextLinkData)
+
+        return r["value"]
+    except Exception as e:
+        logging.error(f"Exception in fetchUserGroups: {e}")
+        return []
 
 
 @app.route('/favicon.ico')
